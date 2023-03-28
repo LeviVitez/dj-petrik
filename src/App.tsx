fb4436207a08a723ecc9f3802ca6aacb562af3b9
LeviVitez <bmwyolo555@gmail.com>
2023-03-28 08:56:57 +0200

    haladas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import NavbarComponent from './components/Navbar-component';
+import Register from './components/pages/Register';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <NavbarComponent></NavbarComponent>
+      <main>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/calendar' element={<Calendar />} />
+          <Route path='/garage' element={<Garage />} />
+          <Route path='/aboutus' element={<AboutUs />} />
+        </Routes>
+      </main>
     </div>
   );
 }
