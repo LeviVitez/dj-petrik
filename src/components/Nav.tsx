1553ba87e36d79c57e82e99a0f0aa03fc3bf8ab4
LeviVitez <bmwyolo555@gmail.com>
2023-03-28 09:59:40 +0200

    nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Nav extends Component {
+    
+    render() {
+        return <nav>
+            <Link to='/'>Főoldal </Link>
+            <Link to='/tapasztalat'>Szakmai tapasztalat </Link>
+            <Link to='/idopont'>Időpontfoglalás </Link>
+        </nav>
+    }
+
+}
